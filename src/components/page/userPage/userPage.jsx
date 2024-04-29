83fe9131ded918e8717fddb371958e35b2ecebad
Marina <perezeginamv@mail.ru>
2024-04-29 11:48:47 +0500

    Setting up comments on a user page

--- src/components/page/userPage/userPage.jsx
+++ src/components/page/userPage/userPage.jsx
@@ -7,156 +7,158 @@ import { useHistory } from "react-router-dom";
 const UserPage = ({ userId }) => {
     const history = useHistory();
     const [user, setUser] = useState();
+    const [comments, setComments] = useState();
     useEffect(() => {
         api.users.getById(userId).then((data) => setUser(data));
+        api.comments
+            .fetchCommentsForUser(userId)
+            .then((comments) => setComments(comments));
     }, []);
+
+    console.log(comments);
     const handleClick = () => {
         history.push(history.location.pathname + "/edit");
     };
     if (user) {
         return (
-            <body>
-                <div className="container">
-                    <div className="row gutters-sm">
-                        <div className="col-md-4 mb-3">
-                            <div className="card mb-3">
-                                <div className="card-body">
-                                    <button
-                                        onClick={handleClick}
-                                        className="
+            <div className="container">
+                <div className="row gutters-sm">
+                    <div className="col-md-4 mb-3">
+                        <div className="card mb-3">
+                            <div className="card-body">
+                                <button
+                                    onClick={handleClick}
+                                    className="
                                     position-absolute
                                     top-0
                                     end-0
                                     btn btn-light btn-sm
                                 "
-                                    >
-                                        <i className="bi bi-gear"></i>
-                                    </button>
-                                    <div
-                                        className="
+                                >
+                                    <i className="bi bi-gear"></i>
+                                </button>
+                                <div
+                                    className="
                                     d-flex
                                     flex-column
                                     align-items-center
                                     text-center
                                     position-relative
                                 "
-                                    >
-                                        <img
-                                            src="https://api.dicebear.com/8.x/adventurer/svg?seed=Samantha"
-                                            alt="avatar"
-                                            width="65"
-                                            height="65"
-                                        />
-                                        <div className="mt-3">
-                                            <h4>{user.name}</h4>
-                                            <p className="text-secondary mb-1">
-                                                {user.profession.name}
-                                            </p>
-                                            <div className="text-muted">
-                                                <i
-                                                    className="
+                                >
+                                    <img
+                                        src="https://api.dicebear.com/8.x/avataaars/svg"
+                                        alt="avatar"
+                                        width="65"
+                                        height="65"
+                                        className="rounded-circle shadow-1-strong me-3"
+                                    />
+                                    <div className="mt-3">
+                                        <h4>{user.name}</h4>
+                                        <p className="text-secondary mb-1">
+                                            {user.profession.name}
+                                        </p>
+                                        <div className="text-muted">
+                                            <i
+                                                className="
                                                 bi bi-caret-down-fill
                                                 text-primary
                                             "
-                                                    role="button"
-                                                ></i>
-                                                <i
-                                                    className="
+                                                role="button"
+                                            ></i>
+                                            <i
+                                                className="
                                                 bi bi-caret-up
                                                 text-secondary
                                             "
-                                                    role="button"
-                                                ></i>
-                                                <span className="ms-2">
-                                                    {user.rate}
-                                                </span>
-                                            </div>
+                                                role="button"
+                                            ></i>
+                                            <span className="ms-2">
+                                                {user.rate}
+                                            </span>
                                         </div>
                                     </div>
                                 </div>
                             </div>
+                        </div>
 
-                            <div className="card mb-3">
-                                <div
-                                    className="
+                        <div className="card mb-3">
+                            <div
+                                className="
                                 card-body
                                 d-flex
                                 flex-column
                                 justify-content-center
                                 text-center
                             "
-                                >
-                                    <h5 className="card-title">
-                                        <span>Qualities</span>
-                                    </h5>
-                                    <p className="card-text">
-                                        <Qualities qualities={user.qualities} />
-                                    </p>
-                                </div>
+                            >
+                                <h5 className="card-title">
+                                    <span>Qualities</span>
+                                </h5>
+                                <p className="card-text">
+                                    <Qualities qualities={user.qualities} />
+                                </p>
                             </div>
+                        </div>
+                        <div className="card mb-3">
                             <div className="card mb-3">
-                                <div className="card mb-3">
-                                    <div
-                                        className="
+                                <div
+                                    className="
                                     card-body
                                     d-flex
                                     flex-column
                                     justify-content-center
                                     text-center
                                 "
-                                    >
-                                        <h5 className="card-title">
-                                            <span>Completed meetings</span>
-                                        </h5>
+                                >
+                                    <h5 className="card-title">
+                                        <span>Completed meetings</span>
+                                    </h5>
 
-                                        <h1 className="display-1">
-                                            {user.completedMeetings}
-                                        </h1>
+                                    <h1 className="display-1">
+                                        {user.completedMeetings}
+                                    </h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-md-8">
+                        <div className="card mb-2">
+                            <div className="card-body">
+                                <div>
+                                    <h2>New comment</h2>
+                                    <div className="mb-4">
+                                        <select
+                                            className="form-select"
+                                            name="userId"
+                                            value=""
+                                        >
+                                            <option disabled value="" selected>
+                                                Выберите пользователя
+                                            </option>
+
+                                            <option>Доктор</option>
+                                            <option>Тусер</option>
+                                        </select>
+                                    </div>
+                                    <div className="mb-4">
+                                        <label
+                                            htmlFor="exampleFormControlTextarea1"
+                                            className="form-label"
+                                        >
+                                            Сообщение
+                                        </label>
+                                        <textarea
+                                            className="form-control"
+                                            id="exampleFormControlTextarea1"
+                                            rows="3"
+                                        ></textarea>
                                     </div>
                                 </div>
                             </div>
                         </div>
-
-                        <div className="col-md-8">
-                            <div className="card mb-2">
-                                <div className="card-body">
-                                    <div>
-                                        <h2>New comment</h2>
-                                        <div className="mb-4">
-                                            <select
-                                                className="form-select"
-                                                name="userId"
-                                                value=""
-                                            >
-                                                <option
-                                                    disabled
-                                                    value=""
-                                                    selected
-                                                >
-                                                    Выберите пользователя
-                                                </option>
-
-                                                <option>Доктор</option>
-                                                <option>Тусер</option>
-                                            </select>
-                                        </div>
-                                        <div className="mb-4">
-                                            <label
-                                                htmlFor="exampleFormControlTextarea1"
-                                                className="form-label"
-                                            >
-                                                Сообщение
-                                            </label>
-                                            <textarea
-                                                className="form-control"
-                                                id="exampleFormControlTextarea1"
-                                                rows="3"
-                                            ></textarea>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
+                        {comments.length > 0 && (
                             <div className="card mb-3">
                                 <div className="card-body">
                                     <h2>Comments</h2>
@@ -228,7 +230,7 @@ const UserPage = ({ userId }) => {
                                                                 modi animi eius
                                                                 voluptatum
                                                                 adipisci amet
-                                                                officiis.
+                                                                officiis.{" "}
                                                             </p>
                                                         </div>
                                                     </div>
@@ -238,10 +240,10 @@ const UserPage = ({ userId }) => {
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        )}
                     </div>
                 </div>
-            </body>
+            </div>
         );
 
         // <div>
@@ -253,7 +255,7 @@ const UserPage = ({ userId }) => {
         //     <button onClick={handleClick}>Изменить</button>
         // </div>
     } else {
-        return <h1>Loading</h1>;
+        return "loading...";
     }
 };
 
