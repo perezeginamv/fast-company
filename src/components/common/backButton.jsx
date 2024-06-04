00b7fa71345acdd51919df108fb6d9cb93ecd2a6
Marina <perezeginamv@mail.ru>
2024-06-04 20:16:35 +0500

    Completion of finalizing the user card. Edits

--- src/components/common/backButton.jsx
+++ src/components/common/backButton.jsx
@@ -3,7 +3,7 @@ import { useHistory } from "react-router";
 
 const BackHistoryButton = () => {
     const history = useHistory();
-    console.log(history);
+
     return (
         <button className="btn btn-primary" onClick={() => history.goBack()}>
             <i className="bi btn-primary"></i>
