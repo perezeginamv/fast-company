135659749ca60f087513f45c3aa0fa9e5615d7c4
Marina <perezeginamv@mail.ru>
2024-06-10 21:24:13 +0500

    fix errors

--- src/components/page/userPage/userPage.jsx
+++ src/components/page/userPage/userPage.jsx
@@ -12,6 +12,7 @@ const UserPage = ({ userId }) => {
     useEffect(() => {
         api.users.getById(userId).then((data) => setUser(data));
     }, []);
+    console.log(user);
 
     if (user) {
         return (
