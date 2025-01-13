72f6737f90f9333d3aab5aa475d439a5bebb5bdb
Marina <perezeginamv@mail.ru>
2025-01-13 19:06:46 +0500

    fix mistakes

--- src/app/components/ui/registerForm.jsx
+++ src/app/components/ui/registerForm.jsx
@@ -95,6 +95,7 @@ const RegisterForm = () => {
             ...data,
             qualities: data.qualities.map((q) => q.value)
         };
+        console.log(newData);
 
         try {
             await signUp(newData);
