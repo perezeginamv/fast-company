b1429200908284444d01f8775888ffa700358aed
Marina <perezeginamv@mail.ru>
2025-01-15 21:02:41 +0500

    User authorization refinement

--- src/app/components/ui/navBar.jsx
+++ src/app/components/ui/navBar.jsx
@@ -1,25 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth";
 
 const NavBar = () => {
+    const { currentUser } = useAuth();
+    console.log(currentUser);
+
     return (
-        <ul className="nav">
-            <li className="nav-item">
-                <Link className=" nav-link" to="/">
-                    Main
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link className=" nav-link" to="/login">
-                    Login
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link className=" nav-link" to="/users">
-                    Users
-                </Link>
-            </li>
-        </ul>
+        <nav className="navbar">
+            <div className="container-fluid">
+                <ul className="nav">
+                    <li className="nav-item">
+                        <Link className=" nav-link" to="/">
+                            Main
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className=" nav-link" to="/login">
+                            Login
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className=" nav-link" to="/users">
+                            Users
+                        </Link>
+                    </li>
+                </ul>
+                <div className="d-flex">
+                    <p>User</p>
+                </div>
+            </div>
+        </nav>
     );
 };
 
