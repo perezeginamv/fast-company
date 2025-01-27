d726c9a316be5725c1b69de1348de8370cfa90b6
Marina <perezeginamv@mail.ru>
2025-01-27 18:53:16 +0500

    fix mistakes

--- src/app/components/page/editUserPage/editUserPage.jsx
+++ src/app/components/page/editUserPage/editUserPage.jsx
@@ -21,6 +21,8 @@ const EditUserPage = () => {
         label: q.name,
         value: q._id
     }));
+    console.log(data);
+
     const { professions, isLoading: professionsLoading } = useProfessions();
     const professionsList = professions.map((p) => ({
         label: p.name,
