c06ec6b50b23787a53212b8c26132cb4880b9263
Marina <perezeginamv@mail.ru>
2025-07-02 19:09:43 +0500

    integration backend and frontend

--- client/src/app/services/qualities.service.js
+++ client/src/app/services/qualities.service.js
@@ -5,7 +5,6 @@ const qualitiesEndpoint = "quality/";
 const qualitiesService = {
     fetchAll: async () => {
         const { data } = await httpService.get(qualitiesEndpoint);
-        console.log(data);
 
         return data;
     }
